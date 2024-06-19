fecad6475e36d4ba1b9341edbc4d1b5d50279a9a
Uncle Jack <unclejackie@proton.me>
2024-06-19 19:57:55 +0330

    refactor config links
    Signed-off-by: Uncle Jack <unclejackie@proton.me>

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,28 @@
 use uuid::Uuid;
 
+pub enum Outbound {
+    Vless,
+    Vmess,
+}
+
+impl Default for Outbound {
+    fn default() -> Self {
+        Self::Vmess
+    }
+}
+
+impl From<String> for Outbound {
+    fn from(s: String) -> Self {
+        match s.as_str() {
+            "VLESS" => Self::Vless,
+            "VMESS" => Self::Vmess,
+            _ => Self::default(),
+        }
+    }
+}
+
 pub struct Config {
     pub uuid: Uuid,
     pub host: String,
-    pub outbound: String,
+    pub outbound: Outbound,
 }
