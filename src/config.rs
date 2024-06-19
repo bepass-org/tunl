c7434885290241e03ebe794e71f8058a163edf6f
Uncle Jack <unclejackie@proton.me>
2024-06-19 18:47:10 +0330

    implement vless protocol
    Signed-off-by: Uncle Jack <unclejackie@proton.me>

--- src/config.rs
+++ src/config.rs
@@ -3,4 +3,5 @@ use uuid::Uuid;
 pub struct Config {
     pub uuid: Uuid,
     pub host: String,
+    pub outbound: String,
 }
--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -1,2 +1,4 @@
+pub mod vless;
 pub mod vmess;
+pub use vless::*;
 pub use vmess::*;
