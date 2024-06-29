c9aa5b0d4653b2a72b384c52574a5b3758149136
Uncle Jack <unclejackie@proton.me>
2024-06-29 17:07:07 +0330

    downgrade worker-rs version due to some runtime warnings
    Signed-off-by: Uncle Jack <unclejackie@proton.me>

--- src/proxy/bepass/encoding.rs
+++ src/proxy/bepass/encoding.rs
@@ -2,6 +2,7 @@ use crate::proxy::Network;
 
 use worker::*;
 
+#[derive(Default)]
 pub struct Header {
     pub network: Network,
     pub address: String,
@@ -9,31 +10,27 @@ pub struct Header {
 }
 
 pub fn decode_request_header(request: &Request) -> Result<Header> {
-    let queries = request.query::<std::collections::HashMap<String, String>>()?;
+    let mut header = Header::default();
 
-    let address = queries
-        .get("host")
-        .ok_or(Error::RustError("invalid request address".to_string()))?;
+    let url = request.url()?;
+    let mut pairs = url.query_pairs();
 
-    let port = {
-        let p = queries
-            .get("port")
-            .ok_or(Error::RustError("invalid request port".to_string()))?;
+    while let Some((k, v)) = pairs.next() {
+        match k.as_ref() {
+            "host" => header.address = v.to_string(),
+            "port" => {
+                header.port = v
+                    .parse::<u16>()
+                    .map_err(|_| Error::RustError("invalid port number".to_string()))?;
+            }
+            "net" => {
+                header.network = Network::from_str(&v)?;
+            }
+            _ => {
+                // ignore other query params for now
+            }
+        }
+    }
 
-        p.parse::<u16>()
-            .map_err(|_| Error::RustError("invalid port number".to_string()))?
-    };
-
-    let network = {
-        let net = queries
-            .get("net")
-            .ok_or(Error::RustError("invalid request network".to_string()))?;
-        Network::from_str(&net)?
-    };
-
-    Ok(Header {
-        address: address.to_string(),
-        network,
-        port,
-    })
+    Ok(header)
 }
